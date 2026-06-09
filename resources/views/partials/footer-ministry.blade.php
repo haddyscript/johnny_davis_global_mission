99efd73bba9f582f854aadc16bb8e6ae6397139a
hadrianfdc <162952181+hadrianfdc@users.noreply.github.com>
2026-06-09 09:02:08 +0800

    email  shows info@johnnydavisministries.org on the ministry site and info@johnnydavisglobalmissions.org on the global missions site

--- resources/views/partials/footer-ministry.blade.php
+++ resources/views/partials/footer-ministry.blade.php
@@ -85,7 +85,11 @@
             <div class="contact-icon">
               <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="currentColor"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
             </div>
-            <a href="mailto:info@johnnydavisglobalmissions.org">info@johnnydavisglobalmissions.org</a>
+            @if($isMinistryPage)
+              <a href="mailto:info@johnnydavisministries.org">info@johnnydavisministries.org</a>
+            @else
+              <a href="mailto:info@johnnydavisglobalmissions.org">info@johnnydavisglobalmissions.org</a>
+            @endif
           </div>
           <div class="contact-row">
             <div class="contact-icon">
